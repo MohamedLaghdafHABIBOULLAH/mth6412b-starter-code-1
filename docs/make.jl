22d0f9c6f3f5e8f422fae644429bdf04d696cfad
MohamedLaghdafHABIBOULLAH <mohamed-laghdaf-2.habiboullah@polymtl.ca>
2024-11-03 22:14:34 -0500

    update make.jl to add repo

--- docs/make.jl
+++ docs/make.jl
@@ -14,6 +14,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/MohamedLaghdafHABIBOULLAH/mth6412b-starter-code-1.git"
+)
